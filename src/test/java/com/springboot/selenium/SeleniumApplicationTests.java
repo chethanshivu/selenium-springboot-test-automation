42feb836c08104b5b4e5dea45e75bf5193007558
shivaprasadks <chethanshivu555@gmail.com>
2024-09-15 18:03:07 +0530

    Added validation

--- src/test/java/com/springboot/selenium/SeleniumApplicationTests.java
+++ src/test/java/com/springboot/selenium/SeleniumApplicationTests.java
@@ -1,12 +1,18 @@
 package com.springboot.selenium;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SeleniumApplicationTests {
+
+	private static final Logger log = LoggerFactory.getLogger(SeleniumApplicationTests.class);
 
 	@Autowired
 	private WebDriver webDriver;
@@ -14,6 +20,7 @@ class SeleniumApplicationTests {
 	@Test
 	void contextLoads() {
         webDriver.get("https://www.orangehrm.com");
+		log.info(webDriver.getTitle());
 	}
 
 }
