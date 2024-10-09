612376610fb074c1c36491507eb8f009c8817446
shivaprasadks <chethanshivu555@gmail.com>
2024-10-09 22:48:38 +0530

    Added cucumber report configuration

--- src/test/java/com/springboot/selenium/SeleniumApplicationTests.java
+++ src/test/java/com/springboot/selenium/SeleniumApplicationTests.java
@@ -1,22 +1,20 @@
 package com.springboot.selenium;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.springboot.selenium.stepdefinitions,"+
-        "com.springboot.selenium.testconfig")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@Slf4j
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME,
+                        value = "com.springboot.selenium.stepdefinitions,"+
+                                "com.springboot.selenium.testconfig")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Regression")
 @SpringBootTest
 class SeleniumApplicationTests {
 
